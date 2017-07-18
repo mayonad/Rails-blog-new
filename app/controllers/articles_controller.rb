4c92beaef669a1652f5aa962de9050df644399e5
Daniel <mayonad@yahoo.com>
2017-07-18 17:20:56 +0800

    first

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,16 @@
+class ArticlesController < ApplicationController
+  def new
+  end
+  end
+
+  class ArticlesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
+
+def create
+  render plain: params[:article].inspect
+end
